749da3777bcd07af91f13a447906624f31787000
zyc <cen66673@163.com>
2022-03-12 23:43:23 +0800

    StackDemo

---
+++ data_structure/src/stru_demo/strack_demo/MyStack.java
@@ -0,0 +1,37 @@
+package stru_demo.strack_demo;
+
+/*
+ * 用数组来实现栈，规定新添加的元素总是在原有数组的后面
+ * 栈低为下标为0的值
+ */
+
+public class MyStack<E> {
+
+    private Object[] obj = new Object[10];
+
+    private int size;
+
+    //规定栈顶和栈底的划分     入栈
+    public void push(E data) {
+        if (size == obj.length) {
+            throw new RuntimeException("栈满......");
+        }
+        obj[size++] = data;
+    }
+
+    //每一次从栈顶出   出栈
+    public E pop() {
+        if (size == 0) {
+            throw new RuntimeException("栈空.....");
+        }
+        return (E) obj[--size];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
---
+++ data_structure/src/stru_demo/strack_demo/TestStack.java
@@ -0,0 +1,101 @@
+package stru_demo.strack_demo;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TestStack {
+
+    public static void main(String[] args) {
+        String a = new Scanner(System.in).nextLine();//231-*
+        ArrayList<Character> arry = new ArrayList<Character>();
+
+        MyStack<Character> stack = new MyStack<Character>();
+
+        String na = "";
+        for (int i = 0; i < 10; i++) {
+            char c = (char) ('0' + i); //ASCII
+//            System.out.println(c);
+            arry.add(c);
+        }
+//        System.out.println(arry.toString());
+
+        for (int i = 0; i < a.length(); i++) {
+            char c = a.charAt(i);
+            //如果是数字
+            if (arry.contains(c)) {
+                na += c;
+            }
+            else {
+                if (stack.isEmpty()) {
+                    stack.push(c);
+                    continue;
+                }
+
+                if (c == '(') {
+                    stack.push(c);
+                    continue;
+                }
+                //是栈之前存放符号位  -
+                char d = stack.pop();//*
+                if (d == '(') {
+                    stack.push(d);
+                    stack.push(c);
+                    continue;
+                }
+                if (c == ')') {
+                    na += d;
+                    char f;
+                    while ((f = stack.pop()) != '(') {
+                        na += f;
+                    }
+                } else {
+                    //c是现在正要比对的符号位  +
+                    int a1 = (d == '+' || d == '-') ? 0 : 1;
+                    int a2 = (c == '+' || c == '-') ? 0 : 1;
+                    if (a1 >= a2) {
+                        na += d;
+                        stack.push(c);
+                    } else if (a1 < a2) {
+                        stack.push(d);
+                        stack.push(c);
+                    }
+                }
+            }
+            if (i == a.length() - 1) {
+                while (!stack.isEmpty()) {
+                    na += stack.pop();
+                }
+            }
+        }
+        MyStack<Integer> ms = new MyStack<Integer>();
+
+        for (int i = 0; i < na.length(); i++) {
+            char c = na.charAt(i);
+
+            if (arry.contains(c)) {
+                ms.push(Integer.parseInt(c + ""));
+            } else {
+                int a1 = ms.pop();
+                int a2 = ms.pop();
+                switch (c) {
+                    case '+':
+                        ms.push(a2 + a1);
+                        break;
+                    case '-':
+                        ms.push(a2 - a1);
+                        break;
+                    case '*':
+                        ms.push(a2 * a1);
+                        break;
+                    case '/':
+                        ms.push(a2 / a1);
+                        break;
+                }
+            }
+
+            if (i == na.length() - 1) {
+                System.out.println(ms.pop());
+            }
+        }
+    }
+}
