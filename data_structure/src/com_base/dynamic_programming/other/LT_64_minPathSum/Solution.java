2a50f74df539205e8d8d90bd54895eb75e05a780
zyc <cen66673@163.com>
2022-05-24 11:06:36 +0800

    64

---
+++ data_structure/src/com_base/dynamic_programming/other/LT_64_minPathSum/Solution.java
@@ -0,0 +1,29 @@
+package com_base.dynamic_programming.other.LT_64_minPathSum;
+
+public class Solution {
+    public static int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        //初始化
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = grid[0][i] + dp[0][i - 1];
+        }
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = grid[i][0] + dp[i - 1][0];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        int[][] nums = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+        System.out.println(Solution.minPathSum(nums));
+    }
+}
