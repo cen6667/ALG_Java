a9b0fd997bd170973b06336dfb4548b373b8d321
zyc <cen66673@163.com>
2022-05-31 11:38:17 +0800

    0531

--- data_structure/src/stru_demo/sort10/sort_06_QuickSort/QuickSort.java
+++ data_structure/src/stru_demo/sort10/sort_06_QuickSort/QuickSort.java
@@ -1,49 +1,48 @@
 package stru_demo.sort10.sort_06_QuickSort;
 
 
+import java.util.Random;
+
 public class QuickSort {
 
-    private static void quickSort(int[] arr, int low, int high) {
+    public void quickSort(int[] nums, int left,int right){
+        if(left>=right) return;
+        int pivotIndex = partition(nums, left, right);
+        quickSort(nums, left, pivotIndex-1);
+        quickSort(nums, pivotIndex+1,right);
+        return;
+    }
+    // 随机种子为当时时间戳
+    private final static Random random = new Random(System.currentTimeMillis());
 
-        if (low < high) {
-            // 找寻基准数据的正确索引
-            int index = getIndex(arr, low, high);
-
-            // 迭代对基准前和基准后的数组进行相同操作
-            quickSort(arr, low, index - 1);
-            quickSort(arr, index + 1, high);
+    private int partition(int[] nums, int left,int right){
+        int randomIndex = left + random.nextInt(right-left+1);
+        swap(nums,left,randomIndex);
+        int pivot = nums[left];
+        //循环不变量（定义不变），初始化保证区间为空
+        // <= pivot [left,j]
+        // > pivot (j,right]
+        int j = left;
+        for(int i = left+1; i<=right;i++){
+            if(nums[i] <= pivot){
+                j++;
+                swap(nums,i,j);
+            }
         }
+        swap(nums, left, j);
+        return j;
     }
 
-    private static int getIndex(int[] arr, int low, int high) {
-        // 基准元素
-        int tmp = arr[low];
-        // low指向队首元素，high指向队尾元素，首尾一起扫描
-        while (low < high) {
-            // 当队尾元素大于等于基准数据时,向前挪动high指针
-            while (low < high && arr[high] >= tmp) {
-                high--;
-            }
-            // 队尾元素小于tmp,则将其赋值给low
-            arr[low] = arr[high];
-
-            // 队首元素小于等于tmp时,向前挪动low指针
-            while (low < high && arr[low] <= tmp) {
-                low++;
-            }
-            // 队首元素大于tmp时,则将其赋值给high
-            arr[high] = arr[low];
-
-        }
-        // 跳出循环时low和high相等,此时的low或high就是tmp的正确索引位置
-        arr[low] = tmp;
-        // 返回tmp的正确位置
-        return low;
+    private void swap(int[] nums, int index1,int index2){
+        int tmp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = tmp;
     }
 
     public static void main(String[] args) {
         int[] arr = {20, 38, 36, 21, 23, 22, 13};
-        quickSort(arr, 0, arr.length - 1);
+        QuickSort qs = new QuickSort();
+        qs.quickSort(arr, 0, arr.length - 1);
         for (int i : arr) {
             System.out.print(i + " ");
         }
---
+++ 笔记/ACM.md
@@ -0,0 +1,51 @@
+# ACM模式需要的方法
+
+# 首先
+
+```java
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+```
+
+
+
+
+
+# 输入
+
+## 创建类
+
+```java
+Scanner s = new Scanner(System.in);
+```
+
+## 数字读取
+
+```
+int count = s.nextInt();
+```
+
+## 字符串读取
+
+```java
+String[] arr = new String[n];
+for(int i = 0;i<n;i++){
+	arr[i]=sc.next();
+}
+```
+
+
+
+
+
+# 输出
+
+
+
+字符串输出
+
+```java
+System.out.println(String.join(" ",arr));
+```
+
